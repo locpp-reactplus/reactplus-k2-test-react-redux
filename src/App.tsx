a69c29a8dddf537c1d36e694a3f95c1a3292737a
datbugs <datbugs807@gmail.com>
2022-02-21 12:20:04 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Exam3
         </a>
       </header>
     </div>
